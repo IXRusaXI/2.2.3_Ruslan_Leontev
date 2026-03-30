e05df9c60a648844fee61a4a242ae70537db2c59
Руслан <moi-denga@yandex.ru>
2026-03-31 02:40:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 import { Circle } from './Circle/Circle';
 
 function App() {
+  // const [activeCircle, setActiveCircle] = useState({})
+
+  const redRef = useRef(null)
+  const orangeRef = useRef(null)
+  const greenRef = useRef(null)
+
+  const activeCircle = useRef()
+
+  const list = [redRef, orangeRef, greenRef]
+
+  const changeActiveCircle = (evt) => {
+    console.log(redRef, orangeRef, greenRef)
+
+    activeCircle.current = evt.target
+    console.log(activeCircle)
+    clearAll()
+  }
+
+  const clearAll = () => {
+    list.map(ref => {
+      if (!!ref) {
+        if (ref !== activeCircle) {
+          
+        }
+      }
+    })
+  }
+
   return (
     <div className="semaphore-container">
       <div className="semaphore-item">
-        <Circle color="red" />
+        <Circle ref={redRef} color="red" active={true} onClick={changeActiveCircle}/>
       </div>
       <div className="semaphore-item">
-        <Circle color="orange" />
+        <Circle ref={orangeRef} color="orange" onClick={changeActiveCircle}/>
       </div>
       <div className="semaphore-item">
-        <Circle color="green" />
+        <Circle ref={greenRef} color="green" onClick={changeActiveCircle}/>
       </div>
     </div>
   );
--- src/Circle/Circle.tsx
+++ src/Circle/Circle.tsx
@@ -1,11 +1,24 @@
 import './Circle.css';
 import classNames from 'classnames';
+import { MutableRefObject, useState, forwardRef } from 'react';
 
-interface Props {
-  color: string;
-  active?: boolean;
-}
+// interface Props {
+//   color: string;
+//   active?: boolean;
+// }
 
-export function Circle({ color, active }: Props) {
-  return <div className="circle circle-green" />;
+export const Circle = forwardRef((props, ref) => {
+  // console.log('Получено в кружочке', props.color, props.active)
+  const [activeState, setActiveState] = useState(props.active)
+
+  const toggleActive = () => {
+    setActiveState(!activeState)
+  }
+
+  return <div ref={ref} onClick={props.onClick} className={classNames(
+    "circle",
+    "circle-" + props.color,
+    activeState && ("circle-" + props.color + "--active")
+  )} />;
 }
+)
